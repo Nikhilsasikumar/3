6b5ce6ba26759b2a00626a7cfb1a78437f75d585
Nikhilsasikumar <nikhilsasikumar1@gmail.com>
2020-09-02 17:47:22 +0530

    updation

--- resources/views/admin/services.blade.php
+++ resources/views/admin/services.blade.php
@@ -10,6 +10,233 @@
 @endsection
 
 @section("page_content")
-<h1>services</h1>
-<a href="/admin/services/table">Click to table</a>
+
+
+
+<!--begin::Notice-->
+<div class="alert alert-custom alert-white alert-shadow fade show gutter-b" role="alert">
+    <div class="alert-icon">
+        <span class="svg-icon svg-icon-primary svg-icon-xl">
+            <!--begin::Svg Icon | path:/metronic/theme/html/demo8/dist/assets/media/svg/icons/Tools/Compass.svg-->
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                    <rect x="0" y="0" width="24" height="24" />
+                    <path d="M7.07744993,12.3040451 C7.72444571,13.0716094 8.54044565,13.6920474 9.46808594,14.1079953 L5,23 L4.5,18 L7.07744993,12.3040451 Z M14.5865511,14.2597864 C15.5319561,13.9019016 16.375416,13.3366121 17.0614026,12.6194459 L19.5,18 L19,23 L14.5865511,14.2597864 Z M12,3.55271368e-14 C12.8284271,3.53749572e-14 13.5,0.671572875 13.5,1.5 L13.5,4 L10.5,4 L10.5,1.5 C10.5,0.671572875 11.1715729,3.56793164e-14 12,3.55271368e-14 Z" fill="#000000" opacity="0.3" />
+                    <path d="M12,10 C13.1045695,10 14,9.1045695 14,8 C14,6.8954305 13.1045695,6 12,6 C10.8954305,6 10,6.8954305 10,8 C10,9.1045695 10.8954305,10 12,10 Z M12,13 C9.23857625,13 7,10.7614237 7,8 C7,5.23857625 9.23857625,3 12,3 C14.7614237,3 17,5.23857625 17,8 C17,10.7614237 14.7614237,13 12,13 Z" fill="#000000" fill-rule="nonzero" />
+                </g>
+            </svg>
+            <!--end::Svg Icon-->
+        </span>
+    </div>
+    <div class="alert-text">Metronic's Iconbox features icons and text with an animated wave
+        effect.</div>
+</div>
+<!--end::Notice-->
+<!--begin::Row-->
+<div class="row">
+
+
+
+    <div class="col-lg-6 col-xl-4 mb-5">
+        <!--begin::Iconbox-->
+        <div class="card card-custom wave wave-animate-slow wave-primary mb-8 mb-lg-0">
+            <div class="card-body">
+                <div class="d-flex align-items-center p-5">
+                    <div class="mr-6">
+                        <span class="svg-icon svg-icon-primary svg-icon-4x">
+                            <!--begin::Svg Icon | path:/metronic/theme/html/demo8/dist/assets/media/svg/icons/Home/Mirror.svg-->
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                    <rect x="0" y="0" width="24" height="24" />
+                                    <path d="M13,17.0484323 L13,18 L14,18 C15.1045695,18 16,18.8954305 16,20 L8,20 C8,18.8954305 8.8954305,18 10,18 L11,18 L11,17.0482312 C6.89844817,16.5925472 3.58685702,13.3691811 3.07555009,9.22038742 C3.00799634,8.67224972 3.3975866,8.17313318 3.94572429,8.10557943 C4.49386199,8.03802567 4.99297853,8.42761593 5.06053229,8.97575363 C5.4896663,12.4577884 8.46049164,15.1035129 12.0008191,15.1035129 C15.577644,15.1035129 18.5681939,12.4043008 18.9524872,8.87772126 C19.0123158,8.32868667 19.505897,7.93210686 20.0549316,7.99193546 C20.6039661,8.05176407 21.000546,8.54534521 20.9407173,9.09437981 C20.4824216,13.3000638 17.1471597,16.5885839 13,17.0484323 Z" fill="#000000" fill-rule="nonzero" />
+                                    <path d="M12,14 C8.6862915,14 6,11.3137085 6,8 C6,4.6862915 8.6862915,2 12,2 C15.3137085,2 18,4.6862915 18,8 C18,11.3137085 15.3137085,14 12,14 Z M8.81595773,7.80077353 C8.79067542,7.43921955 8.47708263,7.16661749 8.11552864,7.19189981 C7.75397465,7.21718213 7.4813726,7.53077492 7.50665492,7.89232891 C7.62279197,9.55316612 8.39667037,10.8635466 9.79502238,11.7671393 C10.099435,11.9638458 10.5056723,11.8765328 10.7023788,11.5721203 C10.8990854,11.2677077 10.8117724,10.8614704 10.5073598,10.6647638 C9.4559885,9.98538454 8.90327706,9.04949813 8.81595773,7.80077353 Z" fill="#000000" opacity="0.3" />
+                                </g>
+                            </svg>
+                            <!--end::Svg Icon-->
+                        </span>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <a href="#" class="text-dark text-hover-primary font-weight-bold font-size-h4 mb-3">Get
+                            Started</a>
+                        <div class="text-dark-75">Lorem Ipsum is simply dummy text of the
+                            printing and typesetting industry. Lorem Ipsum has been the
+                            industry's standard dummy since the 1500s.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--end::Iconbox-->
+        <!--begin::Code example-->
+        <div class="example example-compact mt-2">
+            <div class="example-tools">
+                <span class="example-toggle" data-toggle="tooltip" title="View code"></span>
+                <span class="example-copy" data-toggle="tooltip" title="Copy code"></span>
+            </div>
+            <div class="example-code">
+                <div class="example-highlight">
+                    <pre>
+<code class="language-html">
+&lt;div class="card card-custom wave wave-animate-slow wave-primary mb-8 mb-lg-0"&gt;
+&lt;div class="card-body"&gt;
+&lt;div class="d-flex align-items-center p-5"&gt;
+&lt;div class="mr-6"&gt;
+&lt;span class="svg-icon svg-icon-success svg-icon-4x"&gt;
+&lt;svg&gt;
+...
+&lt;/svg&gt;
+&lt;/span&gt;
+&lt;/div&gt;
+&lt;div class="d-flex flex-column"&gt;
+&lt;a href="#" class="text-dark text-hover-primary font-weight-bold font-size-h4 mb-3"&gt;
+Get Started
+&lt;/a&gt;
+&lt;div class="text-dark-75"&gt;
+...
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+</code>
+</pre>
+                </div>
+            </div>
+        </div>
+        <!--end::Code example-->
+    </div>
+    <div class="col-lg-6 col-xl-4 mb-5">
+        <!--begin::Iconbox-->
+        <div class="card card-custom wave wave-animate wave-danger mb-8 mb-lg-0">
+            <div class="card-body">
+                <div class="d-flex align-items-center p-5">
+                    <div class="mr-6">
+                        <span class="svg-icon svg-icon-danger svg-icon-4x">
+                            <!--begin::Svg Icon | path:/metronic/theme/html/demo8/dist/assets/media/svg/icons/General/Thunder-move.svg-->
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                    <rect x="0" y="0" width="24" height="24" />
+                                    <path d="M16.3740377,19.9389434 L22.2226499,11.1660251 C22.4524142,10.8213786 22.3592838,10.3557266 22.0146373,10.1259623 C21.8914367,10.0438285 21.7466809,10 21.5986122,10 L17,10 L17,4.47708173 C17,4.06286817 16.6642136,3.72708173 16.25,3.72708173 C15.9992351,3.72708173 15.7650616,3.85240758 15.6259623,4.06105658 L9.7773501,12.8339749 C9.54758575,13.1786214 9.64071616,13.6442734 9.98536267,13.8740377 C10.1085633,13.9561715 10.2533191,14 10.4013878,14 L15,14 L15,19.5229183 C15,19.9371318 15.3357864,20.2729183 15.75,20.2729183 C16.0007649,20.2729183 16.2349384,20.1475924 16.3740377,19.9389434 Z" fill="#000000" />
+                                    <path d="M4.5,5 L9.5,5 C10.3284271,5 11,5.67157288 11,6.5 C11,7.32842712 10.3284271,8 9.5,8 L4.5,8 C3.67157288,8 3,7.32842712 3,6.5 C3,5.67157288 3.67157288,5 4.5,5 Z M4.5,17 L9.5,17 C10.3284271,17 11,17.6715729 11,18.5 C11,19.3284271 10.3284271,20 9.5,20 L4.5,20 C3.67157288,20 3,19.3284271 3,18.5 C3,17.6715729 3.67157288,17 4.5,17 Z M2.5,11 L6.5,11 C7.32842712,11 8,11.6715729 8,12.5 C8,13.3284271 7.32842712,14 6.5,14 L2.5,14 C1.67157288,14 1,13.3284271 1,12.5 C1,11.6715729 1.67157288,11 2.5,11 Z" fill="#000000" opacity="0.3" />
+                                </g>
+                            </svg>
+                            <!--end::Svg Icon-->
+                        </span>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <a href="#" class="text-dark text-hover-primary font-weight-bold font-size-h4 mb-3">Tutorials</a>
+                        <div class="text-dark-75">Lorem Ipsum is simply dummy text of the
+                            printing and typesetting industry. Lorem Ipsum has been the
+                            industry's standard dummy since the 1500s.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--end::Iconbox-->
+        <!--begin::Code example-->
+        <div class="example example-compact mt-2">
+            <div class="example-tools">
+                <span class="example-toggle" data-toggle="tooltip" title="View code"></span>
+                <span class="example-copy" data-toggle="tooltip" title="Copy code"></span>
+            </div>
+            <div class="example-code">
+                <div class="example-highlight">
+                    <pre>
+<code class="language-html">
+&lt;div class="card card-custom wave wave-animate wave-danger mb-8 mb-lg-0"&gt;
+&lt;div class="card-body"&gt;
+&lt;div class="d-flex align-items-center p-5"&gt;
+&lt;div class="mr-6"&gt;
+&lt;span class="svg-icon svg-icon-danger svg-icon-4x"&gt;
+&lt;svg&gt;
+...
+&lt;/svg&gt;
+&lt;/span&gt;
+&lt;/div&gt;
+&lt;div class="d-flex flex-column"&gt;
+&lt;a href="#" class="text-dark text-hover-primary font-weight-bold font-size-h4 mb-3"&gt;
+Tutorials
+&lt;/a&gt;
+&lt;div class="text-dark-75"&gt;
+...
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+</code>
+</pre>
+                </div>
+            </div>
+        </div>
+        <!--end::Code example-->
+    </div>
+    <div class="col-lg-6 col-xl-4">
+        <!--begin::Iconbox-->
+        <div class="card card-custom wave wave-animate-fast wave-success">
+            <div class="card-body">
+                <div class="d-flex align-items-center p-5">
+                    <div class="mr-6">
+                        <span class="svg-icon svg-icon-success svg-icon-4x">
+                            <!--begin::Svg Icon | path:/metronic/theme/html/demo8/dist/assets/media/svg/icons/Design/Sketch.svg-->
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                    <rect x="0" y="0" width="24" height="24" />
+                                    <polygon fill="#000000" opacity="0.3" points="5 3 19 3 23 8 1 8" />
+                                    <polygon fill="#000000" points="23 8 12 20 1 8" />
+                                </g>
+                            </svg>
+                            <!--end::Svg Icon-->
+                        </span>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <a href="#" class="text-dark text-hover-primary font-weight-bold font-size-h4 mb-3">User
+                            Guide</a>
+                        <div class="text-dark-75">Lorem Ipsum is simply dummy text of the
+                            printing and typesetting industry. Lorem Ipsum has been the
+                            industry's standard dummy since the 1500s.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--end::Iconbox-->
+        <!--begin::Code example-->
+        <div class="example example-compact mt-2">
+            <div class="example-tools">
+                <span class="example-toggle" data-toggle="tooltip" title="View code"></span>
+                <span class="example-copy" data-toggle="tooltip" title="Copy code"></span>
+            </div>
+            <div class="example-code">
+                <div class="example-highlight">
+                    <pre>
+<code class="language-html">
+&lt;div class="card card-custom wave wave-animate-fast wave-success"&gt;
+&lt;div class="card-body"&gt;
+&lt;div class="d-flex align-items-center p-5"&gt;
+&lt;div class="mr-6"&gt;
+&lt;span class="svg-icon svg-icon-success svg-icon-4x"&gt;
+&lt;svg&gt;
+...
+&lt;/svg&gt;
+&lt;/span&gt;
+&lt;/div&gt;
+&lt;div class="d-flex flex-column"&gt;
+&lt;a href="#" class="text-dark text-hover-primary font-weight-bold font-size-h4 mb-3"&gt;
+User Guide
+&lt;/a&gt;
+&lt;div class="text-dark-75"&gt;
+...
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+</code>
+</pre>
+                </div>
+            </div>
+        </div>
+        <!--end::Code example-->
+    </div>
+</div>
+<!--end::Row-->
 @endsection
